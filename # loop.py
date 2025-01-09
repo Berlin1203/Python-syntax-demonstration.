6a3fca2c2e4d6e1fa6e7eb9f2780bf08bcd3f374
Alok <140788661+Berlin1203@users.noreply.github.com>
2025-01-09 20:34:59 +0530

    Add files via upload

---
+++ # loop.py
@@ -0,0 +1,11 @@
+# For loop
+print("Fruits in the list:")
+for fruit in fruits:
+    print(fruit)
+
+# While loop
+count = 0
+print("Counting to 5:")
+while count < 5:
+    print(count)
+    count += 1  # Increment count by 1
