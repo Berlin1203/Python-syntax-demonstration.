fa60523e9cf933eb632b46892eb3fb13d00a1942
Alok <140788661+Berlin1203@users.noreply.github.com>
2025-01-09 20:37:43 +0530

    Add files via upload

---
+++ # Variables and Data Types.py
@@ -0,0 +1,11 @@
+# Variables and Data Types
+name = "Alice"  # String
+age = 30        # Integer
+height = 5.5    # Float
+is_student = True  # Boolean
+
+# Print function
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
