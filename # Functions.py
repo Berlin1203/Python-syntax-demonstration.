71ed25b03a2cf8f74ae7a659e699e1aaa7504f9d
Alok <140788661+Berlin1203@users.noreply.github.com>
2025-01-09 20:34:02 +0530

    Add files via upload

---
+++ # Functions.py
@@ -0,0 +1,16 @@
+# Functions
+def greet(person_name):
+    """This function greets a person with their name."""
+    print("Hello,", person_name)
+
+# Calling the function
+greet(name)
+
+# Returning values from functions
+def add_numbers(a, b):
+    """This function returns the sum of two numbers."""
+    return a + b
+
+# Using the function
+result = add_numbers(5, 7)
+print("Sum of 5 and 7 is:", result)
